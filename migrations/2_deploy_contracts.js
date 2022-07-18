fbcd5515b63fe49ab14d0dfb8db39c5859fa9478
energy-on-chain <matthew@energyonchain.net>
2022-07-18 11:00:25 -0500

    Updated smart contract cid for ipfs

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,8 +2,8 @@ const OpenEmoji = artifacts.require("OpenEmoji")
 
 module.exports = async function (deployer) {
 
-    // const IPFS_IMAGE_METADATA_URI = `ipfs://${process.env.REACT_APP_IPFS_IMAGE_METADATA_CID}/`
-    const IPFS_IMAGE_METADATA_URI = `https://gateway.pinata.cloud/ipfs/${process.env.REACT_APP_IPFS_IMAGE_METADATA_CID}/` 
+    const IPFS_IMAGE_METADATA_URI = `ipfs://${process.env.REACT_APP_IPFS_IMAGE_METADATA_CID}/`
+    // const IPFS_IMAGE_METADATA_URI = `https://gateway.pinata.cloud/ipfs/${process.env.REACT_APP_IPFS_IMAGE_METADATA_CID}/` 
     const IPFS_HIDDEN_IMAGE_METADATA_URI = `ipfs://${process.env.REACT_APP_IPFS_HIDDEN_IMAGE_METADATA_CID}/hidden.json`
     const NFT_MINT_DATE = new Date(process.env.REACT_APP_NFT_MINT_DATE).getTime().toString().slice(0, 10)
 
